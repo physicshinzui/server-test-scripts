3cd9d7fa2e6e9004b4284cda0bf89aef34f07d25
physicshinzui <physicshinzui@gmail.com>
2024-03-10 19:40:00 +0900

    add lines for host cuda ver and container cuda ver

--- 05_gromacs_gpu_frm_def/01_do.sh
+++ 05_gromacs_gpu_frm_def/01_do.sh
@@ -2,4 +2,7 @@
 set -eu 
 
 #singularity build --nv --fakeroot --sandbox gmx_gpu gmx_gpu.def
-singularity build --nv --fakeroot gmx_gpu.sif gmx_gpu.def
+
+# Main
+singularity build --nv --fakeroot gmx_gpu_hostcuda.sif gmx_gpu.def
+singularity build --nvccli --fakeroot gmx_gpu_contcuda.sif gmx_gpu.def
