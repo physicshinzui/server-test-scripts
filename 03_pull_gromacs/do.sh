268140675d89e476fac612eac7dc944c93a9824c
physicshinzui <physicshinzui@gmail.com>
2024-03-10 20:08:36 +0900

    delete 03_dir

--- 03_pull_gromacs/do.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash 
-set -eu 
-
-# singularity pull docker://nvcr.io/hpc/gromacs:2023.2
-
-singularity pull docker://nvcr.io/hpc/gromacs:2018.2
--- 03_pull_gromacs/workflow.sh
+++
@@ -1,4 +0,0 @@
-/#!/bin/bash 
-
-
-singuilarity run --bind 1536:/mnt gromacs_2018.2.sif gmx grompp 
